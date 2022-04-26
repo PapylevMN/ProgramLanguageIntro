78cca5ddeedbba7b7d1c69ebc3fcc04f466c7f6f
Mikhail <a104s@mail.ru>
2022-04-26 16:05:37 +0300

    Добавил ДЗ про собаку

---
+++ Dog_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Enter Distanse :");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Friend 1 Speed :");
+int friendOneSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Friend 2 Speed :");
+int friendTwoSpeed = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Dog Speed :");
+int dogSpeed = Convert.ToInt32(Console.ReadLine());
+
+int count = 0; 
+int friend = 1;
+double time = 0;
+
+while(distance > 1)
+{
+    if(friend > 0)
+    {
+    time = distance/(friendOneSpeed+dogSpeed);
+    }
+    else
+    {
+    time = distance/(friendTwoSpeed+dogSpeed);
+    }
+    
+    distance = distance-(friendOneSpeed + friendTwoSpeed) * time;
+    //Console.WriteLine(distance);
+    count++;
+    friend = friend *(-1);
+}
+Console.Write("Dog will shuttle :");
+Console.WriteLine(count);
--- Example_005/Program.cs
+++ Example_005/Program.cs
@@ -1,5 +1,7 @@
-﻿Console.Write("Введите имя пользователя :");
-string username = Console.ReadLine();
+﻿string username;
+Console.Write("Введите имя пользователя :");
+username = Console.ReadLine();
+
 if(username.ToLower() == "маша")
 {
      Console.WriteLine("Ура, это же Маша!");
